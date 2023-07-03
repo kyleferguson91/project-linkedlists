554a3cff3e99ca5b24afd279f1dbab4e17cf1bbd
Kyle Ferguson <kyleferguson91@gmail.com>
2023-07-03 00:45:42 -0400

    added size tail head append prepend functions to linked list

--- linkedlist.js
+++ linkedlist.js
@@ -8,15 +8,15 @@ return {
        
         // we want to append to the end of the linked list, well we need to find the end, which exists when we find the first item with a next reference to null, this could be as simple as this.head.next or in other words the first item
         // although if this.head.value is null, then we add it here.
-if (this.head.value == null) {
+if (this.list.head.value == null) {
     // no value present, there is no list, add it here..
-    this.head = node(value)
+    this.list.head = node(value)
 
 }
 
 // below, the value exists (so we have an item) so check the next pointer, while it is not equal to null, keep going, once we find one equal to null, we know we have reached the end of our list and can append our node      
        else {
-        let current = this.head
+        let current = this.list.head
         while(current.next) {
             // 
             // until we reach a null value for the next pointer, keep going
@@ -32,50 +32,49 @@ if (this.head.value == null) {
 
 
     prepend: function(value) {
-        // want to add to start of list.  what if head points to null?  then we don't need to update the next pointer..
-        if (this.head.value == null) {
-            // head points to an empty list, add it here, don't need to update anything else 
-       
-            this.head = node(value)
+        // in the event we have no value in first item.. 
+        if (!this.list.head.value) {
+            // there is no item in spot one.. add the node
+            this.list.head = node('added')
         }
-        else if (this.head.value && !this.head.next) {
+        // we only want to add it to the front of the list, 
+        // we store the current item in old reference, 
+        // and we set the 
+        let oldref = this.list.head
+        this.list.head = node(value)
+        this.list.head.next = oldref
 
-            //head points to something, but it is only one item in list so we must add it here and update the pointer..
-            // we want to set this.head.next to be equal to the current value of this.head, and then overwrite this.head with our prepend value
-
-            this.head.next = this.head
-            this.head = node(value)
+    },
+    
+    size: function(node = this.list.head, count = 0) {
+        // assuming that a 'node' contains both a data and next field
+        if (node.next == null) {
+            if (node.value) {   count++}
+            return count
         }
-        else {
-                // otherwise the head has a value (we have item 1), the the next pointer is not a null reference, we want to shift everything down 1..
+        count++
+       return     this.size(node.next, count)
+    },
 
-                let current = this.head
-                while (current.next) {
-                    // while head.next is defind, keep moving things down one, once done we will overwrite the initial value
-                    
-                    // set the next link in chain equal to the current link
-                    current.next = current
-                    // advance the loop by 1 
-                    current = current.next
+    head: function() {
+        return this.list.head   
+    },
+    
+    tail: function(node = this.list.head) {
 
-                }
-                current.next = null
-                // once we are done moving each link 'down by one' we can finially prepend our value..
-                this.head = node(value)
+        // start with head of list and recurse until we reach tail or in otherwords we reach a next pointer of null
 
-        }
-        
-
+        if (node.next == null) {return node}
+       return this.tail(node.next)
 
     },
     
     
 
-    head: {
-        value: '1',
-        next: null
-
-    }
+    list: { head: {
+        
+      
+    }}
 
 
 }
@@ -84,13 +83,19 @@ if (this.head.value == null) {
 
 const dog = linkedList()
 
-dog.prepend(22)
-console.log(dog.head)
-
+//dog.append(22)
+//console.log(dog.list.head)
+//console.log(dog.tail(), 'tailfn')
+dog.append(1)
+dog.append(122)
+dog.append(3)
+dog.append(17)
+dog.append(3)
+console.log(dog.tail().value)
 function node(value) {
 
     return {
 value: value,
-next: null
+next: this.next || null
     }
 }
